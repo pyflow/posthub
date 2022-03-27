2bb737264730afaad4f40983e77a874a2911c373
Zhuo Wei <zeaphoo@qq.com>
2022-03-27 08:20:20 +0800

    WIP: fix schema.sql

--- posthub/sql/schema.sql
+++ posthub/sql/schema.sql
@@ -43,22 +43,11 @@ CREATE TABLE if not exists public.posthub_queue (
 WITH (fillfactor='90');
 
 CREATE INDEX posthub_queue_poll_idx
-    ON posthub_queue (que_name, scheduled_at, id)
+    ON posthub_queue (que_name, scheduled_at, msg_schema_version, id)
     WHERE (state in ('created', 'active', 'retry'));
 
-CREATE UNLOGGED TABLE if not exists public.posthub_queue_listener (
-    pid integer NOT NULL,
-    queues text[] NOT NULL,
-    listening boolean NOT NULL,
-    msg_schema_version integer DEFAULT 1,
-    created timestamptz not null default now(),
-    updated timestamptz not null default now(),
-    CONSTRAINT valid_queues CHECK (((array_ndims(queues) = 1) AND (array_length(queues, 1) IS NOT NULL)))
-)
-WITH (fillfactor='90');
 
-
-CREATE FUNCTION que_determine_job_state(job public.posthub_queue, max_retry integer) RETURNS text AS $$
+CREATE FUNCTION posthub_que_determine_job_state(job public.posthub_queue, max_retry integer) RETURNS text AS $$
   SELECT
     CASE
     WHEN job.completed_at IS NOT NULL    THEN 'completed'
@@ -73,7 +62,7 @@ CREATE FUNCTION que_determine_job_state(job public.posthub_queue, max_retry inte
 $$
 LANGUAGE SQL;
 
-CREATE FUNCTION que_state_notify() RETURNS trigger AS $$
+CREATE FUNCTION posthub_que_state_notify() RETURNS trigger AS $$
   DECLARE
     row record;
     message json;
@@ -82,15 +71,15 @@ CREATE FUNCTION que_state_notify() RETURNS trigger AS $$
   BEGIN
     IF TG_OP = 'INSERT' THEN
       previous_state := 'nonexistent';
-      current_state  := public.que_determine_job_state(NEW);
+      current_state  := public.posthub_que_determine_job_state(NEW);
       row            := NEW;
     ELSIF TG_OP = 'DELETE' THEN
-      previous_state := public.que_determine_job_state(OLD);
+      previous_state := public.posthub_que_determine_job_state(OLD);
       current_state  := 'nonexistent';
       row            := OLD;
     ELSIF TG_OP = 'UPDATE' THEN
-      previous_state := public.que_determine_job_state(OLD);
-      current_state  := public.que_determine_job_state(NEW);
+      previous_state := public.posthub_que_determine_job_state(OLD);
+      current_state  := public.posthub_que_determine_job_state(NEW);
 
       -- If the state didn't change, short-circuit.
       IF previous_state = current_state THEN
@@ -117,53 +106,50 @@ CREATE FUNCTION que_state_notify() RETURNS trigger AS $$
         current_state  AS current_state
     ) t;
 
-    PERFORM pg_notify('que_state', message::text);
+    PERFORM pg_notify('posthub_que_state', message::text);
 
     RETURN null;
   END
 $$
 LANGUAGE plpgsql;
 
-CREATE TRIGGER que_state_notify
-  AFTER INSERT OR UPDATE OR DELETE ON que_jobs
+CREATE TRIGGER posthub_que_state_notify
+  AFTER INSERT OR UPDATE OR DELETE ON posthub_queue
   FOR EACH ROW
-  EXECUTE PROCEDURE public.que_state_notify();
+  EXECUTE PROCEDURE public.posthub_que_state_notify();
 
 
-CREATE FUNCTION que_job_notify() RETURNS trigger AS $$
+CREATE FUNCTION posthub_que_msg_notify() RETURNS trigger AS $$
   DECLARE
-    locker_pid integer;
     sort_key json;
   BEGIN
-    IF locker_pid IS NOT NULL THEN
-      -- There's a size limit to what can be broadcast via LISTEN/NOTIFY, so
-      -- rather than throw errors when someone enqueues a big job, just
-      -- broadcast the most pertinent information, and let the locker query for
-      -- the record after it's taken the lock. The worker will have to hit the
-      -- DB in order to make sure the job is still visible anyway.
-      SELECT row_to_json(t)
-      INTO sort_key
-      FROM (
-        SELECT
-          'msg_available' AS message_type,
-          NEW.que_name       AS que_name,
-          NEW.id          AS message_id,
-          -- Make sure we output timestamps as UTC ISO 8601
-          to_char(NEW.enqueued_at AT TIME ZONE 'UTC', 'YYYY-MM-DD"T"HH24:MI:SS.US"Z"') AS enqueued_at
-      ) t;
+    -- There's a size limit to what can be broadcast via LISTEN/NOTIFY, so
+    -- rather than throw errors when someone enqueues a big job, just
+    -- broadcast the most pertinent information, and let the locker query for
+    -- the record after it's taken the lock. The worker will have to hit the
+    -- DB in order to make sure the job is still visible anyway.
+    SELECT row_to_json(t)
+    INTO sort_key
+    FROM (
+    SELECT
+        'msg_available' AS message_type,
+        NEW.que_name       AS que_name,
+        NEW.id          AS message_id,
+        -- Make sure we output timestamps as UTC ISO 8601
+        to_char(NEW.enqueued_at AT TIME ZONE 'UTC', 'YYYY-MM-DD"T"HH24:MI:SS.US"Z"') AS enqueued_at
+    ) t;
 
-      PERFORM pg_notify('que_listener_' || locker_pid::text, sort_key::text);
-    END IF;
+    PERFORM pg_notify('posthub_queue_' || NEW.que_name::text, sort_key::text);
 
     RETURN null;
   END
 $$
 LANGUAGE plpgsql;
 
-CREATE TRIGGER que_job_notify
-  AFTER INSERT ON que_jobs
+CREATE TRIGGER posthub_que_msg_notify
+  AFTER INSERT ON posthub_queue
   FOR EACH ROW
-  EXECUTE PROCEDURE public.que_job_notify();
+  EXECUTE PROCEDURE public.posthub_que_msg_notify();
 
 CREATE FUNCTION que_try_get_nowait(que_name text) RETURNS posthub_message AS $$
   DECLARE
